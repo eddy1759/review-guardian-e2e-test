d46d914581d83121e219272f6a97642ba400d625
Edet Emmanuel Asuquo <59479466+eddy1759@users.noreply.github.com>
2025-07-21 13:44:58 +0100

    Create testing.js

---
+++ src/testing.js
@@ -0,0 +1,8 @@
+// A new feature file
+
+// TODO: This is a test comment that should be flagged by Review Guardian.
+
+function main() {
+  // TODO: user name must be added
+  return "hello world";
+}
